9114b344afda44c600cfa9703eb0d3418baff8b4
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2017-11-23 01:11:45 +0100

    Update step.sh

--- step.sh
+++ step.sh
@@ -9,7 +9,7 @@ mkdir -p docs
 rm -rf "docs/$BITRISE_APP_TITLE"
 
 set +e
-composer_dir_name=$(find "$BITRISE_SOURCE_DIR" -path \*/composer/\*index.html | xargs dirname)
+composer_dir_name=$(find "$BITRISE_SOURCE_DIR" -path \*/composer/\*index.html | xargs dirname)/..
 if [ -z "$composer_dir_name" ]; then
       echo -n "unknown" | envman add --key ALL_TEST_COUNT
       echo -n 0 | envman add --key PASSED_TEST_COUNT
@@ -20,7 +20,7 @@ set -e
 
 jq -n "$STF_DEVICE_SERIAL_LIST | length" | envman add --key STF_DEVICE_COUNT
 
-mv ${composer_dir_name}/.. "docs/$BITRISE_APP_TITLE"
+mv ${composer_dir_name} "docs/$BITRISE_APP_TITLE"
 git add -A
 git commit -am "$BITRISE_APP_TITLE $BITRISE_BUILD_NUMBER"
 git push origin master
