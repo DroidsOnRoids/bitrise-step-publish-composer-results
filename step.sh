0f68085fdcfd4239cf9556675c60055f9937d6df
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2018-03-19 03:52:22 +0100

    Remove test counters

--- step.sh
+++ step.sh
@@ -11,8 +11,6 @@ rm -rf "docs/$BITRISE_APP_TITLE"
 set +e
 composer_html_dir_name=$(find "$BITRISE_SOURCE_DIR" -path \*/composer/\*index.html | xargs dirname)
 if [ -z "$composer_html_dir_name" ]; then
-      echo -n "unknown" | envman add --key ALL_TEST_COUNT
-      echo -n 0 | envman add --key PASSED_TEST_COUNT
       echo -n "irrelevant" | envman add --key STF_DEVICE_COUNT
       exit 1
 fi
@@ -25,6 +23,3 @@ mv ${composer_dir_name} "docs/$BITRISE_APP_TITLE"
 git add -A
 git commit -am "$BITRISE_APP_TITLE $BITRISE_BUILD_NUMBER"
 git push origin master
-
-echo -n "unknown" | envman add --key ALL_TEST_COUNT
-echo -n "unknown" | envman add --key PASSED_TEST_COUNT
